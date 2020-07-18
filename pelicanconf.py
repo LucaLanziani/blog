9dd56a57b12904bc43ea431ffaec79284a96f022
Luca Lanziani <luca.lanziani@gmail.com>
2020-07-18 17:16:00 +0200

    Update pelican and site

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-import sys
 from pelican.settings import DEFAULT_CONFIG
 
 
-
-AUTHOR = u'Luca Lanziani'
-SITENAME = u'Luca Lanziani'
-SITEURL = 'http://127.0.0.1:8000'
+AUTHOR = 'Luca Lanziani'
+SITENAME = 'Luca Lanziani'
+SITEURL = ''
 
 PATH = 'content'
 
+TIMEZONE = 'Europe/Paris'
 
-TIMEZONE = 'Europe/Rome'
-
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -25,7 +22,6 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 THEME = "./pelican-themes/pure"
-
 PLUGIN_PATHS = ['./pelican-plugins']
 PLUGINS = ['gravatar', 'series', 'sitemap', 'related_posts', 'neighbors',
            'liquid_tags.img', 'liquid_tags.video', 'liquid_tags.youtube',
@@ -35,16 +31,17 @@ PLUGINS = ['gravatar', 'series', 'sitemap', 'related_posts', 'neighbors',
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),)
 
+
 # Social widget
 SOCIAL = (('github', 'https://github.com/LucaLanziani'),
           ('twitter', 'http://twitter.com/lucalanziani'),
           ('linkedin', 'http://uk.linkedin.com/in/lucalanziani/'),
           ('envelope', 'mailto:luca.lanziani+site@gmail.com'),)
 
-DEFAULT_PAGINATION = 3
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+DEFAULT_PAGINATION = 10
 
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
 SITEMAP = {
     'format': 'xml',
     'priorities': {
@@ -82,24 +79,3 @@ EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
     'extra/favicon.ico': {'path': 'favicon.ico'}
 }
-
-#
-# Move content around
-# All the blog related contents are created inside blog/ subdirectory
-# the pages are outside the blog subfolder inside the pages/ directory
-#
-
-BLOG_PREFIX = 'blog/'
-
-module = sys.modules[__name__]
-# ENTITIES_SETTING
-for entity in ['tag', 'category', 'author', 'article', 'draft']:
-    url_key = "%s_URL" % entity.upper()
-    save_as_key = "%s_SAVE_AS" % entity.upper()
-    setattr(module, url_key, BLOG_PREFIX + DEFAULT_CONFIG[url_key])
-    setattr(module, save_as_key, BLOG_PREFIX + DEFAULT_CONFIG[save_as_key])
-
-# DIRECT_TEMPLATES_SETTING
-for direct_template in DEFAULT_CONFIG['DIRECT_TEMPLATES']:
-    save_as_key = "%s_SAVE_AS" % direct_template.upper()
-    setattr(module, save_as_key, BLOG_PREFIX + '%s.html' % direct_template)
